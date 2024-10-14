58efd7097c6761173d82708a7664a8c521967b91
Soto Mateo <Masoto@uade.edu.ar>
2024-10-14 18:21:54 -0300

    Optimización de los menues

--- menues/menues.py
+++ menues/menues.py
@@ -1,47 +1,36 @@
-from tabulate import tabulate
 from variables import constantes as cs
 from funciones import funcionesX as fx, mensajes as fm, productos as fp, clientes as fc
-
+from termcolor import colored
+import time
 
 def menu_opciones(menu_desplegar: int) -> int:
-    fx.clear_console()
+    """
+    Está función muestra las opciones del menu que haya seleccionado el usuario y devuelve el siguiente menu
+    Pre: Está función recibe como parametro en formato entero que representa el menu seleccionado
+    Post: Está funcion devuelve un entero que representa el siguiente menu
+    """
+
     while True:
+        fx.clear_console()
+        fx.mostrar_logo()
+        fx.mostrar_opciones(cs.dict_opciones, menu_desplegar)
         try:
-            print(
-                tabulate(
-                    [["Sistema de avisos ACQUABIO"], ["by Cocucha"]],
-                    tablefmt="fancy_grid",
-                    colalign=("center",),
-                )
-            )
-            fx.mostrar_opciones(cs.dict_opciones, menu_desplegar)
             option = int(input("Seleccione una de las opciones: "))
             if not (option in cs.dict_opciones[menu_desplegar].keys()):
-                raise ValueError("Opción ingresada no valida")
+                raise ValueError()
             break
-        except ValueError as e:
-            print(f"{e}")
-            fx.clear_console()
+        except ValueError:
+            print(colored("Opción ingresada no valida...", "red"))
+            time.sleep(1)
     return option
 
 
-def menu_principal():
-    option_select = menu_opciones(0)
-    match option_select:
-        case 1:
-            menu_clientes()
-        case 2:
-            menu_mensajes()
-        case 3:
-            menu_producto()
-        case 4:
-            exit()
-        case _:
-            print("Opción no valida")
-    return None
-
-
-def menu_clientes():
+def menu_clientes()->None:
+    """
+    Está función muestra las opciones para un crud (crear un cliente, leer datos del cliente, modificar un cliente y borrar cliente)
+    Pre: Está función no recibe ningun parametro
+    Post: Está función no devuelve nada
+    """
     option_select = menu_opciones(1)
     match option_select:
         case 1:
@@ -56,12 +45,15 @@ def menu_clientes():
             fc.mostrar_clientes()
         case 6:
             menu_principal()
-        case _:
-            print("Opción no valida")
-    return None
 
 
-def menu_producto():
+def menu_producto()->None:
+    """
+    Está función muestra las opciones para un crud (crear un producto, leer datos de un producto,
+    modificar un producto y borrar producto)
+    Pre: Está función no recibe ningun parametro
+    Post: Está función no devuelve nada
+    """
     option_select = menu_opciones(2)
     match option_select:
         case 1:
@@ -76,12 +68,15 @@ def menu_producto():
             fp.ver_productos()
         case 6:
             menu_principal()
-        case _:
-            print("Opción no valida")
-    return None
 
 
-def menu_mensajes():
+def menu_mensajes()->None:
+    """
+    Está función muestra las opciones para un crud (crear un mensaje, leer datos de un mensaje,
+    modificar un mensaje y borrar mensaje)
+    Pre: Está función no recibe ningun parametro
+    Post: Está función no devuelve nada
+    """
     option_select = menu_opciones(3)
     match option_select:
         case 1:
@@ -96,6 +91,21 @@ def menu_mensajes():
             fm.ver_mensajes()
         case 6:
             menu_principal()
-        case _:
-            print("Opción no valida")
-    return None
+
+
+def menu_principal()->None:
+    """
+    Está función es el menu principal donde se inicia el programa, desde aca pasa a los siguientes menues
+    Pre: Está función no recibe ningun parametro
+    Post: Está función no devuelve nada
+    """
+    option_select = menu_opciones(0)
+    match option_select:
+        case 1:
+            menu_clientes()
+        case 2:
+            menu_mensajes()
+        case 3:
+            menu_producto()
+        case 4:
+            exit()
