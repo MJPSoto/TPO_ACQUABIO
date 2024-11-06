f9d092c528618a90140e5fb06c8b666081ab7e15
Franco Aller <faller@uade.edu.ar>
2024-11-05 21:08:24 -0300

    avances en las funciones, falta terminar de corregir la sintaxis

--- funciones/mensajes.py
+++ funciones/mensajes.py
@@ -6,10 +6,38 @@ import re
 RUTA = "JSON/mensajes.json"
 
 
-def mensaje_valido(texto):
-    # Patrón: al menos cuatro caracteres, contiene letras, no es solo números ni solo espacios
+def mensaje_valido(texto) -> bool:
+    """
+    Recibe un texto y comprueba si es valido o no
+
+    pre: recive un string
+
+    post: devuelve un booleno
+    """
+    #al menos cuatro caracteres, contiene letras, no es solo números ni solo espacios
     patron = r"^(?=.*[A-Za-z])(?=.{4,})(?!^\d+$).*$"
     return bool(re.match(patron, texto.strip()))
+
+def volver_menu()-> None:
+    """
+    Pregunta si quiere volver al menú. Te lleva al menú principal si ingresa "y" y te lleva al menu mensajes
+    si ingresa "n".
+
+    pre: no recibe nada
+
+    post: no devuelve nada
+    """
+    ok = input("¿Desea volver al menú principal? (y/n): ").lower()
+    if ok == "y":
+        menu.menu_principal()
+    elif ok == "n":
+        menu.menu_mensajes()
+    else:
+        print("Valor ingresado incorrecto")
+        #si ingresa un valor ingorrecto vuelve a iniciar la función
+        volver_menu()
+    return None
+
 
 def obtener_datos_mensaje() -> list[str]:
     """
@@ -20,7 +48,7 @@ def obtener_datos_mensaje() -> list[str]:
     post: devuelve una lista con los datos
     """
     while True:
-        mensaje = [""]
+        mensaje = []
         nuevo_mensaje = input("Ingrese nuevo mensaje: ")
         #verifico si el mensaje es valido y no es demasiado largo
         if mensaje_valido(nuevo_mensaje):
@@ -34,8 +62,15 @@ def obtener_datos_mensaje() -> list[str]:
                     ok = input("¿Los datos ingresados son correctos? (y/n): ").lower()
                     if ok == "y":
                         mensaje = [dias, nuevo_mensaje]
+                        return mensaje
+                    elif ok == "n":
+                        continue
+                    else:
+                        print("Valor ingresado incorrecto")
             except ValueError:
                 print("Datos ingresados son incorrectos")
+        else:
+            print("El mensaje ingresado no es valido")
         return mensaje
     
     
@@ -50,62 +85,84 @@ def crear_mensaje() -> None:
     """
     #leo el json y lo guardo en la variable mansajes
     mensajes = fx.leer_JSON(RUTA)
-    print(mensajes)
     nuevo_mensaje = obtener_datos_mensaje()
     #defino los dias desde la lista devuelta en la linea anterior
-    dias = nuevo_mensaje[0]
-    #defino los mensajes de la misma manera
-    mensaje = nuevo_mensaje[1]
-    #copruebo si el mensaje ya existe
-    if mensajes[0].get(dias) is None:
-        #creo el mensaje nuevo
-        mensajes[0][dias] = mensaje
-        print("Mensaje cargado.")
-    else:
-        #compruebo si quiere reescribir el mensaje
-        ok = input("El ID de mensaje ya existe quiere reemplazarlo (y/n): ").lower()
-        if ok == "y":
+    try:    
+        dias = nuevo_mensaje[0]
+        #defino los mensajes de la misma manera
+        mensaje = nuevo_mensaje[1]
+        #copruebo si el mensaje ya existe
+        if mensajes[0].get(dias) is None:
+            #creo el mensaje nuevo
             mensajes[0][dias] = mensaje
             print("Mensaje cargado.")
         else:
-            #esto no se si dejarlo asi o volver a la carga de mensajes
-            print("No se creó ningun mensaje...")
-    #reescribo el json
-    with open(RUTA, "w") as archivo:
-        json.dump(mensajes, archivo, indent=4)
-    menu.menu_mensajes()
+            #compruebo si quiere reescribir el mensaje
+            ok = input("El ID de mensaje ya existe quiere reemplazarlo (y/n): ").lower()
+            if ok == "y":
+                mensajes[0][dias] = mensaje
+                ver_mensajes()
+                print("Mensaje cargado.")
+            else:
+                #esto no se si dejarlo asi o volver a la carga de mensajes
+                print("No se creó ningun mensaje...")
+        #reescribo el json
+        with open(RUTA, "w") as archivo:
+            json.dump(mensajes, archivo, indent=4)
+        menu.menu_mensajes()
+    except IndexError as e:
+        print(f"Error: {e}")
+        crear_mensaje()
     return None
 
 
 def actualizar_mensaje() -> None:
     """
-    Obtine el mensaje nuevo a travez de la funcion obtener_datos_mensaje, busca la clave
+    Obtine el mensaje nuevo a travez de la funcion obtener_datos_mensaje, busca la dias
     que es la cantidad de dias, y si está modifica el mensaje
 
     pre: no recive nada
 
     port: no devuelve nada
     """
-    #obtengo el menssaje nuevo
-    nuevo_mensaje = obtener_datos_mensaje()
-    #defino los dias desde la lista devuelta en la linea anterior
-    dias = nuevo_mensaje[0]
-    #defino los mensajes de la misma manera
-    mensaje = nuevo_mensaje[1]
+    ver_mensajes()
     #leo el json
     mensajes = fx.leer_JSON(RUTA)
-    #comparo para ver si la clave(los dias) existen en el json
-    if mensajes[0].get(dias) is None:
-        #si no encuentra el mensaje solo imprime un aviso
-        print("Mensaje no encotrado")
-    else:
-        #creo el mensaje nuevo
-        mensajes[0][dias] = mensaje
-        print("Mensaje cargado.")
-    #vuelvo a cargar todo en el json
-    with open(RUTA, "w") as archivo:
-        json.dump(mensajes, archivo, indent=4)
-    menu.menu_mensajes()
+    #obtengo el menssaje nuevo
+    nuevo_mensaje = obtener_datos_mensaje()
+    print(nuevo_mensaje)
+    try: ############# verifica el uso del try, los try van donde sabes que va a fallar tu codigo 
+        #defino los dias desde la lista devuelta en la linea anterior
+        dias = nuevo_mensaje[0]
+        #defino los mensajes de la misma manera
+        mensaje = nuevo_mensaje[1]
+        #comparo para ver si la dias(los dias) existen en el json
+        while True:    
+            if mensajes[0].get(dias) is None: #####################ojo el codigo flecha
+                #compruebo si quiere crear el mensaje
+                ok = input("El ID de mensaje no existe, desea crearlo (y/n): ").lower()
+                if ok == "y":
+                    mensajes[0][dias] = mensaje
+                    print("mensaje cargado.")
+                    break
+                elif ok == "n": ########################## si ya comprobaste que no sea y no hace falta que vulvas a preguntar si es n
+                    #esto no se si dejarlo asi o volver a la carga de mensajes##############
+                    print("No se creó ningun mensaje...")
+                    break
+                else:
+                    print("Opcion incorrecta")
+            else:
+                #actualizo el valor del mensaje
+                mensajes[0][dias] = mensaje
+                print("mensaje cargado.")
+                break
+        #vuelvo a cargar todo en el json
+        with open(RUTA, "w") as archivo:
+            json.dump(mensajes, archivo, indent=4)
+        menu.menu_mensajes()
+    except IndexError as e:
+        print(f"Error: {e}")
+        actualizar_mensaje()
     return None
 
 
@@ -119,36 +176,40 @@ def borrar_mensaje() -> None:
 
     prost: no devuelve nada
     """
+    ver_mensajes()
     #defino los dias desde la lista devuelta en la linea anterior
-    try:
-        dias = input("Ingrese la cantidad de días: ")
-        #verifico que la cantidad de dias sea valida
-        if int(dias) > 0 and int(dias) <= 100:
-            #leo el json
-            mensajes = fx.leer_JSON(RUTA)
-            #comparo para ver si la clave(los dias) existen en el json
-            if mensajes[0].get(dias) is None:
-                #si no encuentra ningun mensaje
-                print("Mensaje no encontrado")
-            else:
-                #creo el mensaje nuevo
-                print(mensajes[0][dias])
-                ok = input("Este es  el mensaje que queres eliminar? (y/n): ").lower()
-                if ok == "y":
-                    del mensajes[0][dias]
-                    # Vuelvo a cargar todo en el JSON
-                    with open(RUTA, "w") as archivo:
-                        json.dump(mensajes, archivo, indent=4)
-                    print("Mensaje eliminado.")
-                    menu.menu_mensajes()
-                else:
-                    print("Mensaje no eliminado")
-                    menu.menu_mensajes()
-        else:
-            print("Número ingresado fuera de rango")
-    except ValueError:
-        print("El valor ingresado es invalido")
-    return None
+    dias = input("Ingrese la cantidad de días: ") ######################## upa acá no puede dar error, cuando se castea un input y ese casteo no se puede hacer, eso genera un error
+    #verifico que la cantidad de dias sea valida
+    if not re.match("\b([1-9][0-9]{0,2})\b", dias):####################### trata de usar retorno rapido y no usar los ifs en flecha ######cambie el >1 <100 por una expresion regular
+        return "Candidad de dias invalida"       
+        
+    #leo el json
+    mensajes = fx.leer_JSON(RUTA)
+
+    mensaje = mensajes[0].get(dias)
+            
+    #comparo para ver si la dias(los dias) existen en el json
+    if mensaje is None:
+    #si no encuentra ningun mensaje
+        volver_menu()
+        return "Mensaje no encontrado" ### tenemos que devolver cosas no printear cosas 
+
+    print(mensaje)
+    ok = input("Es este el mensaje que desea eliminar? (y/n): ").lower()
+    if ok == "n":
+        return "No se modificó ningun mensaje"        
+
+    if ok != "y":
+        volver_menu()
+        return "Valor incorrecto"
+
+    del mensajes[0][dias]
+        
+    # Vuelvo a cargar todo en el JSON
+    with open(RUTA, "w") as archivo:
+        json.dump(mensajes, archivo, indent=4)
+    return "Mensaje eliminado."
+        
 
 def ver_mensajes() -> None:
     """
@@ -161,7 +222,9 @@ def ver_mensajes() -> None:
     mensajes = fx.leer_JSON(RUTA)
     for key, value in mensajes[0].items():
         print(f"Dias: {key}- mensaje: {value}")
+    volver_menu()
     return None
+
 
 def ver_mensaje() -> None:
     """
@@ -173,10 +236,10 @@ def ver_mensaje() -> None:
     """
     mensajes = fx.leer_JSON(RUTA)
     try:
-        dias = input("Ingrese la cantidad de días: ")
+        dias = input("Ingrese la cantidad de días: ") ############################# lo mismo acá que en la anterior función 
         #verifico que la cantidad de dias sea valida
-        if int(dias) > 0 and int(dias) <= 100:
-            #comparo para ver si la clave(los dias) existen en el json
+        if int(dias) > 0 and int(dias) <= 100: ##################### ojo con el codigo flecha
+            #comparo para ver si la dias(los dias) existen en el json
             try:
                 if mensajes[0].get(dias) is None:
                     print("Mensaje no encontrado")
@@ -184,7 +247,14 @@ def ver_mensaje() -> None:
                     print(f"Dias: {dias}- mensaje: {mensajes[0][dias]}")
             except KeyError as e:
                 print(f"Error: {e}")
+                menu.menu_mensajes()
         else:
             print("Cantidad de dias ingresada no valida.")
+            volver_menu()
     except ValueError:
         print("El valor ingresado es invalido")
+        volver_menu()
+    except IndexError as e:
+        print(f"Error: {e}")
+        volver_menu()
+    return None
--- funciones/productos.py
+++ funciones/productos.py
@@ -1,23 +1,134 @@
 from variables import constantes as cs
 from funciones import funcionesX as fx
+from menues import menues as menu
 import random as rn
 import json
 import re
 
 RUTA = "JSON/productos.json"
 
-def crear_producto() -> None:
+def producto_valido(texto) -> bool:
     """
-    Crea id de producto de manera aleatoria y pide al usuario que ingrese las caracteristicas
-    restantes del producto.
+    Recibe un texto y comprueba si es valido o no
+
+    pre: recive un string
+
+    post: devuelve un booleno
+    """
+    # Patrón: al menos cuatro caracteres, contiene letras, no es solo números ni solo espacios
+    patron = r"^(?=.*[A-Za-z])(?=.{4,})(?!^\d+$).*$"
+    return bool(re.match(patron, texto.strip()))
+
+
+def obtener_datos_producto() -> list[str]:
+    """
+    Esta funcion toma los datos del producto y los valida
 
     pre: no recive nada
 
+    post: devuelve una lista con los datos
+    """
+    while True:
+        producto = []
+        nuevo_producto = input("Ingrese nuevo producto: ")
+        #verifico si el producto es valido y no es demasiado largo
+        if producto_valido(nuevo_producto):
+            try:
+                clave = input("Ingrese la cantidad de días: ")
+                #verifico que la cantidad de clave sea valida
+                if int(clave) > 0 and int(clave) <= 100:
+                    print(f"El producto es: {nuevo_producto}")
+                    #verificación de que el producto ingresado es correcto
+                    ok = input("¿Los datos ingresados son correctos? (y/n): ").lower()
+                    if ok == "y":
+                        producto = [clave, nuevo_producto]
+                        return producto
+                    elif ok == "n":
+                        continue
+                    else:
+                        print("Valor ingresado incorrecto")
+            except ValueError:
+                print("Datos ingresados son incorrectos")
+        else:
+            print("El producto ingresado no es valido")
+        return producto
+
+
+def datos_producto_nuevo() -> list[str]:
+    """
+    Esta funcion toma los datos del producto nuevo y los valida
+
+    pre: no recive nada
+
+    post: devuelve una lista con los datos
+    """
+    while True:
+        productos = fx.leer_JSON(RUTA)
+        producto = []
+        nuevo_producto = input("Ingrese descripcion de nuevo producto: ")
+        #verifico si el texto de producto es valido
+        if producto_valido(nuevo_producto):
+            clave = int(max((producto for producto in productos[0]), default=0)) +1
+            print(f"La descripcion del producto es: {nuevo_producto}")
+            #verificación de que el producto ingresado es correcto
+            ok = input("¿Los datos ingresados son correctos? (y/n): ").lower()
+            if ok == "y":
+                producto = [clave, nuevo_producto]
+                return producto
+            elif ok == "n":
+                continue
+            else:
+                print("Valor ingresado incorrecto")
+        else:
+            print("Datos ingresados son incorrectos")
+            return producto
+
+
+def crear_producto() -> None:
+    """
+    Esta funcion toma los datos, comprueba si son validos y los agrega al json
+    
+    pre: no recibe nada
+
     post: no devuelve nada
     """
+    #leo el json y lo guardo en la variable productos
+    productos = fx.leer_JSON(RUTA)
+    nuevo_producto = datos_producto_nuevo()
+    #defino la clave de la lista devuelta en la linea anterior
+    try:    
+        clave = nuevo_producto[0]
+        #defino los productos de la misma manera
+        producto = nuevo_producto[1]
+        #copruebo si el producto ya existe
+        if productos[0].get(clave) is None:
+            #creo el producto nuevo
+            productos[0][clave] = producto
+            print("producto cargado.")
+        else:
+            #compruebo si quiere reescribir el producto
+            ok = input("El ID de producto ya existe desea reemplazarlo (y/n): ").lower()
+            if ok == "y":
+                productos[0][clave] = producto
+                print("producto cargado.")
+            else:
+                #esto no se si dejarlo asi o volver a la carga de productos##############
+                print("No se creó ningun producto...")
+        #reescribo el json
+        with open(RUTA, "w") as archivo:
+            json.dump(productos, archivo, indent=4)
+        menu.menu_producto()
+    except IndexError as e:
+        print(f"Error: {e}")
+        menu.menu_producto()
+    except FileNotFoundError:
+        print("No se encontró el archivo")
+        menu.menu_producto()
+    except KeyboardInterrupt:
+        print("Se interrumpio el proceso")
+        menu.menu_producto()
+    return None
     
-    
-    pass
 
 def actualizar_producto() -> None:
     """
@@ -28,50 +139,138 @@ def actualizar_producto() -> None:
 
     post: devuelve un dicccionario
     """
-    #imprime lista de claves de productos
-    [print(producto) for producto in cs.lista_claves]
-    #ingresa una clade de producto
-    clave_producto = input("\nIngrese clave producto: ")
-    #comprueba si la clave esta en el diccionario 
-    if clave_producto in cs.stock_productos:
-        #ingresa valor a cambiar como entero
-        cantidad_stock = int(input("Ingrese el stock actual: "))
-        #compueba que sea valido el stock
-        if 0 <= cantidad_stock <= 200:
-            #convierte el valor a string y lo cambia por el nuevo
-            cs.stock_productos[clave_producto] = str(cantidad_stock)
-        else:
-            print("Valor de stock incorrecto")
-    else:
-        print("La clave ingresada no pertenece a ningun producto")
+    ver_productos()
+    #obtengo el menssaje nuevo
+    nuevo_producto = obtener_datos_producto()
+    try:    
+        #defino los clave desde la lista devuelta en la linea anterior
+        clave = nuevo_producto[0]
+        #defino los productos de la misma manera
+        producto = nuevo_producto[1]
+        #leo el json
+        productos = fx.leer_JSON(RUTA)
+        #compruebo que la clave existe
+        while True:    
+            if productos[0].get(clave) is None:
+                #compruebo si quiere crear el producto
+                ok = input("El ID de producto no existe, desea crearlo (y/n): ").lower()
+                if ok == "y":
+                    productos[0][clave] = producto
+                    print("producto cargado.")
+                    break
+                elif ok == "n":
+                    #esto no se si dejarlo asi o volver a la carga de productos##############
+                    print("No se creó ningun producto...")
+                    break
+                else:
+                    print("Opcion incorrecta")
+            else:
+                #actualizo el valor del producto
+                productos[0][clave] = producto
+                print("producto cargado.")
+                break
+        #vuelvo a cargar todo en el json
+        with open(RUTA, "w") as archivo:
+            json.dump(productos, archivo, indent=4)
+        menu.menu_producto()
+    except IndexError as e:
+        print(f"Error: {e}")
+        actualizar_producto()
+    except FileNotFoundError:
+        print("No se encontró el archivo")
+        menu.menu_producto()
+    except KeyboardInterrupt:
+        print("Se interrumpio el proceso")
+        menu.menu_producto()
     return None
 
 
 def borrar_producto():
     """
-    Actualizar mensaje con ese id
-    """
-    pass
+    Busca el producto por el Id ingresado por el usuario y lo elimina
 
-def ver_productos():
+    pre: no recive nada
+
+    prost: no devuelve nada
     """
-    Actualizar mensaje con ese id
-    """
-    productos = fx.leer_JSON(RUTA)
-    for key, value in productos[0].items():
-        print(f"Dias: {key}- mensaje: {value}")
+    ver_productos()
+    try:
+        clave = input("Ingrese la clave de producto: ")
+        #verifico que la clave sea valida
+        if int(clave) > 0 and int(clave) <= 1000:
+            #leo el json
+            productos = fx.leer_JSON(RUTA)
+            #obtengo la clave y verifico que existen en el json
+            if productos[0].get(clave) is None:
+                print("Producto no encontrado")
+            else:
+                #imprimo para verificar si es correcto
+                print(productos[0][clave])
+                ok = input("Este es el producto que desea eliminar? (y/n): ").lower()
+                if ok == "y":
+                    del productos[0][clave]
+                    # Vuelvo a cargar todo en el JSON
+                    with open(RUTA, "w") as archivo:
+                        json.dump(productos, archivo, indent=4)
+                    print("producto eliminado.")
+                    menu.menu_producto()
+                else:
+                    print("producto no eliminado")
+                    menu.menu_producto()
+        else:
+            print("Número ingresado fuera de rango")
+    except ValueError:
+        print("El valor ingresado es invalido")
+    except IndexError as e:
+        print(f"Error: {e}")
+        menu.menu_producto()
+    except FileNotFoundError:
+        print("No se encontró el archivo")
+        menu.menu_producto()
+    except KeyboardInterrupt:
+        print("Se interrumpio el proceso")
+        menu.menu_producto()
     return None
 
 
-def ver_producto():
-    """
-    Actualizar mensaje con ese id
-    """
-    pass
 
-def crear_archivo_productos(dict_productos: dict):
-    file = open("../CSV/archivo_productos.csv", "a+", encoding="utf-8")
-    for value in dict_productos.values():
-        cadena = ",".join(value) + "\n"
-        file.write(cadena)
+def ver_productos():
+    """
+    Actualizar producto con ese id
+    """
+    try:
+        productos = fx.leer_JSON(RUTA)
+        for key, value in productos[0].items():
+            print(f"clave: {key}- producto: {value}")
+    except IndexError as e:
+        print(f"Error: {e}")
+    return None
+
+
+
+def ver_producto() -> None:
+    """
+    Muestra en pantalla el producto con el id ingresado por teclado
+
+    pre: no recibe nada
+
+    post: no devuelve nada
+    """
+    productos = fx.leer_JSON(RUTA)
+    try:
+        clave = input("Ingrese la clave de producto: ")
+        #verifico que la clave de producto sea valida
+        if int(clave) > 0 and int(clave) <= 1000:
+            #obtengo la clave y verifico que existen en el json
+            try:
+                if productos[0].get(clave) is None:
+                    print("Clave de producto no encontrada")
+                else:
+                    print(f"clave: {clave}- producto: {productos[0][clave]}")
+            except KeyError as e:
+                print(f"Error: {e}")
+        else:
+            print("La clave ingresada no es valida.")
+    except ValueError:
+        print("El valor ingresado es invalido")
     return None
